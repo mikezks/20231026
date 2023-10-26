3128426a20d1b2492210e0ff6c0a0541457497d0
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-26 17:34:30 +0200

    feat: signals demo

--- libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
+++ libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
 import { CityPipe } from '@flight-demo/shared/ui-common';
 import { Flight, FlightService } from '@flight-demo/tickets/domain';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-flight-search',
@@ -13,10 +15,20 @@ import { Flight, FlightService } from '@flight-demo/tickets/domain';
   imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
 })
 export class FlightSearchComponent {
-  from = 'London';
-  to = 'New York';
+  from = signal('London');
+  to = signal('New York');
   flights: Array<Flight> = [];
   selectedFlight: Flight | undefined;
+
+  from$ = toObservable(this.from).pipe(
+    debounceTime(3_000)
+  );
+  debouncedFrom = toSignal(this.from$, {
+    initialValue: this.from()
+  });
+  flightRoute = computed(
+    () => 'From ' + this.debouncedFrom() + ' to ' + this.to() + '.'
+  );
 
   basket: Record<number, boolean> = {
     3: true,
@@ -25,15 +37,21 @@ export class FlightSearchComponent {
 
   private flightService = inject(FlightService);
 
+  constructor() {
+    effect(
+      () => console.log(this.flightRoute())
+    );
+  }
+
   search(): void {
-    if (!this.from || !this.to) {
+    if (!this.from() || !this.to()) {
       return;
     }
 
     // Reset properties
     this.selectedFlight = undefined;
 
-    this.flightService.find(this.from, this.to).subscribe({
+    this.flightService.find(this.from(), this.to()).subscribe({
       next: (flights) => {
         this.flights = flights;
       },
